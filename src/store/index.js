374c43f97abdb5971dc662e917f05868f5db964b
Cristina Kukhar <criskukhar@hotmail.com>
2022-12-26 00:25:03 +0100

    redux devtools extension installed

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import {createStore, combineReducers} from 'redux';
-//import { composeWithDevTools } from '@redux-devtools/extension';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 import * as reducers from './reducers';
 
 const reducer = combineReducers(reducers);
 
 export default function configureStore(preloadedState){
-    const store = createStore(reducer, preloadedState/* ,composeWithDevTools */)
+    const store = createStore(reducer, preloadedState,composeWithDevTools() )
 
     return store;
 }
